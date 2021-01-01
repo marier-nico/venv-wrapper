7c11ec7feb18dc036c0964df2767c38bf3be120b
Nicolas Marier <software@nmarier.com>
2021-01-01 14:55:58 -0500

    Load configuration values from a file or the env

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,31 @@
 extern crate clap;
 #[macro_use]
 extern crate eyre;
+#[macro_use]
+extern crate serde;
 
+use eyre::Result;
 use std::io::Write;
-use simple_eyre::eyre::Result;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 mod clap_app;
 mod commands;
+mod settings;
 
 fn main() {
     let result = cli_main();
     if let Err(e) = result {
         let mut stdout = StandardStream::stdout(ColorChoice::Always);
-        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red))).ok();
+        stdout
+            .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+            .ok();
         writeln!(&mut stdout, "\nError: {:?}", e).ok();
     }
 }
 
 fn cli_main() -> Result<()> {
     simple_eyre::install()?;
+    let settings = settings::Settings::new()?;
 
     let matches = clap_app::get_app().get_matches();
     match matches.subcommand() {
---
+++ src/settings.rs
@@ -0,0 +1,47 @@
+use std::path::PathBuf;
+
+use directories::UserDirs;
+use eyre::Result;
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    venvs_directory: PathBuf,
+}
+
+impl Settings {
+    pub fn new() -> Result<Self> {
+        let mut settings = config::Config::new();
+
+        // Set the default venvs directory
+        if let Some(dir) = UserDirs::new() {
+            let mut venvs_default_dir = dir.home_dir().to_owned();
+            venvs_default_dir.push(".virtualenvs");
+
+            settings
+                .set_default(
+                    "venvs_directory",
+                    venvs_default_dir
+                        .to_str()
+                        .ok_or_else(|| eyre!("Path to home directory is not valid UTF-8"))?,
+                )
+                .ok();
+        } else {
+            return Err(eyre!("Could not determine the path to your home directory"));
+        }
+
+        // It's fine to unwrap here because if no home directory path is found, an Err() will be
+        // returned when setting the default value just above.
+        let dir = directories::ProjectDirs::from("com", "venv-wrapper", "venv-wrapper").unwrap();
+        let mut config_file = dir.preference_dir().to_owned();
+        config_file.push("config.toml");
+        settings.merge(config::File::from(config_file)).ok();
+
+        settings
+            .merge(config::Environment::with_prefix("VENVWRAPPER"))
+            .ok();
+
+        Ok(settings
+            .try_into()
+            .map_err(|_| eyre!("The provided configuration is in an invalid format"))?)
+    }
+}
