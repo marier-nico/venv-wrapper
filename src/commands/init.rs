475fca8c826464134e73ed7483d1c8890afd2d82
Nicolas Marier <software@nmarier.com>
2021-01-01 14:54:35 -0500

    Format the init command

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -4,7 +4,8 @@ use eyre::Context;
 
 pub fn init() -> eyre::Result<()> {
     let current_exe = current_exe().context("Could not determine current executable path")?;
-    let s = format!("function venv () {{
+    let s = format!(
+        "function venv () {{
         result=\"$({} $@)\"
 
         if [[ \"$result\" == SHELLEVAL* ]]; then
@@ -12,7 +13,11 @@ pub fn init() -> eyre::Result<()> {
         else
             echo -e $result
         fi
-    }}", current_exe.to_str().ok_or_else(|| eyre!("Path to executable is not valid UTF-8"))?);
+    }}",
+        current_exe
+            .to_str()
+            .ok_or_else(|| eyre!("Path to executable is not valid UTF-8"))?
+    );
 
     print!("{}", s);
     Ok(())
