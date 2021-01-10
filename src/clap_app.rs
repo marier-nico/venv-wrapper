c2401743135cd575518cfe2ef64b3b6d3e6b12c4
Nicolas Marier <software@nmarier.com>
2021-01-10 16:24:33 -0500

    Add long about texts

--- src/clap_app.rs
+++ src/clap_app.rs
@@ -13,6 +13,12 @@ pub fn get_app<'a, 'b>() -> clap::App<'a, 'b> {
 
         (@subcommand init =>
             (about: "Prepare the shell for use")
+            (long_about: 
+                "Output a shell function to STDOUT to allow venv-wrapper to modify the shell state. \
+                The function that is output and evaluated essentially runs the venv-wrapper \
+                binary and evals the contents of a file that is written to by venv-wrapper itself (to modify the shell's state). \
+                Feel free to audit the function by running the `init` command without evaluating the result."
+            )
         )
         (@subcommand ls =>
             (about: "Show a list of available virtualenvs")
@@ -31,13 +37,27 @@ pub fn get_app<'a, 'b>() -> clap::App<'a, 'b> {
         )
         (@subcommand rm =>
             (about: "Remove a virtualenv")
+            (long_about: 
+                "Remove a virtualenv. The currently active virtualenv cannot be removed with this method. \
+                You should deactivate the current virtualenv first, if that is the one you would like to remove. \
+                Also, to make accidental data loss more difficult, only a directory containg a `bin` \
+                folder, which itself contains an `activate` file will be removed by this command."
+            )
             (@arg venv_name: +required +takes_value "The name of the virtualenv to delete")
         )
         (@subcommand project =>
             (about: "Manage projects associated with virtualenvs")
+            (long_about:
+                "Manage what working directory is associated with a virtualenv to quickly switch \
+                between which project you are currently working on."
+            )
             (setting: AppSettings::ArgRequiredElseHelp)
             (@subcommand link =>
                 (about: "Link a project (directory) to a given virtualenv")
+                (long_about:
+                    "Associate a working directory with a virtualenv. Multiple runs of this command \
+                    will overwrite previously set values."
+                )
                 (@arg project_dir: !required +takes_value -p --("project-dir") "The path to the project's directory [default: ./]")
                 (@arg venv_name: +required +takes_value "The name of the virtualenv to link to a project")
             )
@@ -49,6 +69,12 @@ pub fn get_app<'a, 'b>() -> clap::App<'a, 'b> {
         (@subcommand use_command =>
             (name: "use")
             (about: "Use a virtualenv (activate it, and move to its directory)")
+            (long_about: 
+                "Use a virtualenv (begin working on the project associated with it, if any). \
+                If no project is associated with the virtualenv, it is only activated. \
+                If a project is associated with it, the current shell will move to the project's \
+                directory and the virtualenv will be activated."
+            )
             (@arg venv_name: +required +takes_value "The name of the virtualenv to use")
         )
     )
