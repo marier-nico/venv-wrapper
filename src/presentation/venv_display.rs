9900d7680b8ca77b7dbc8c0d245ac138385e7850
Nicolas Marier <software@nmarier.com>
2021-07-17 11:37:49 -0400

    feat(venv-display): add the linked project to the output

--- src/presentation/venv_display.rs
+++ src/presentation/venv_display.rs
@@ -7,13 +7,21 @@ use crate::virtualenv::virtualenv_info::Virtualenv;
 impl Display for Virtualenv {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let name: &str = self.name.as_ref();
+
         writeln!(f, "{}", Style::new().bold().paint(name))?;
         writeln!(
             indented(f),
             "Python Version: {}",
             Style::new().bold().paint(self.python_version.patch_prefix())
         )?;
-        write!(indented(f), "Venv Directory: {}", self.path().to_string_lossy())?;
+        writeln!(indented(f), "Venv Directory: {}", self.path().to_string_lossy())?;
+
+        write!(indented(f), "Linked Project: ")?;
+        if self.linked_project.is_some() {
+            write!(f, "{}", self.linked_project.as_ref().unwrap().to_string_lossy())?;
+        } else {
+            write!(f, "None")?;
+        }
 
         Ok(())
     }
