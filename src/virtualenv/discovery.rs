e0ab2cb1576378eaad373ef4b47eb109511eb48e
Nicolas Marier <software@nmarier.com>
2021-07-17 11:20:57 -0400

    feat(discovery): use rust-ini to parse venv config

--- src/virtualenv/discovery.rs
+++ src/virtualenv/discovery.rs
@@ -1,10 +1,6 @@
 use eyre::{eyre, Context, Result};
-use std::{
-    convert::TryFrom,
-    fs::{self, File},
-    io::{BufRead, BufReader},
-    path::Path,
-};
+use ini::Ini;
+use std::{convert::TryFrom, fs, path::Path};
 
 use crate::virtualenv::creation::python_version::PythonVersion;
 
@@ -12,30 +8,26 @@ use super::virtualenv_info::Virtualenv;
 
 pub fn find_in_path(name: &str, parent_dir: &Path) -> Result<Virtualenv> {
     let venv_config_file = parent_dir.join(format!("{}/pyvenv.cfg", name));
-    if venv_config_file.exists() {
-        let file =
-            File::open(venv_config_file).wrap_err("Could not open the venv's config file")?;
+    let config =
+        Ini::load_from_file(venv_config_file).wrap_err("Could not load the venv config")?;
 
-        for line in BufReader::new(file).lines().flatten() {
-            let (key, value) = match line.split_once('=') {
-                Some(cfg) => cfg,
-                None => continue,
-            };
+    let venv_version = config.get_from::<&str>(None, "version");
+    let virtualenv_version = config.get_from::<&str>(None, "version_info");
 
-            if key.trim() == "version" || key.trim() == "version_info" {
-                let version = PythonVersion::try_from(value)
-                    .wrap_err("Could not determine the python version of the venv")?;
+    let version = match (venv_version, virtualenv_version) {
+        (Some(v), _) => v,
+        (None, Some(v)) => v,
+        (None, None) => return Err(eyre!("Python version not present in venv config")),
+    };
 
-                return Ok(Virtualenv {
-                    name: name.to_string(),
-                    parent_dir: parent_dir.to_owned(),
-                    python_version: version,
-                });
-            }
-        }
-    }
+    let version =
+        PythonVersion::try_from(version).wrap_err("Could not parse the python version")?;
 
-    Err(eyre!("Could not find a virtual environment named '{}' in {:?}", name, parent_dir))
+    Ok(Virtualenv {
+        name: name.to_string(),
+        parent_dir: parent_dir.to_owned(),
+        python_version: version,
+    })
 }
 
 pub fn find_all_in_path(parent_dir: &Path) -> Vec<Virtualenv> {
