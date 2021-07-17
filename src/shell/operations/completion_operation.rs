011e3bb7d96fd5c20b5a671c31374be12c3a0d84
Nicolas Marier <software@nmarier.com>
2021-07-17 19:00:11 -0400

    feat(completions): update completions for link, unlink and use

--- src/shell/operations/completion_operation.rs
+++ src/shell/operations/completion_operation.rs
@@ -22,10 +22,12 @@ impl ShellOperation for CompletionOperation {
 
             _venv_wrapper_completions() {{
                 if [ "${{#COMP_WORDS[@]}}" = "2" ]; then
-                    COMPREPLY=($(compgen -W "activate help ls new rm" -- "${{COMP_WORDS[1]}}"))
+                    COMPREPLY=($(compgen -W "activate help link ls new rm unlink use" -- "${{COMP_WORDS[1]}}"))
                 fi
 
                 if [ "${{COMP_WORDS[1]}}" = "activate" ]; then
+                    COMPREPLY=($(compgen -W "$(ls {0})" -- "${{COMP_WORDS[${{COMP_CWORD}}]}}"))
+                elif [ "${{COMP_WORDS[1]}}" = "link" ]; then
                     COMPREPLY=($(compgen -W "$(ls {0})" -- "${{COMP_WORDS[${{COMP_CWORD}}]}}"))
                 elif [ "${{COMP_WORDS[1]}}" = "new" ]; then
                     if [ "${{#COMP_WORDS[@]}}" = "3" ]; then
@@ -41,6 +43,10 @@ impl ShellOperation for CompletionOperation {
                     fi
                 elif [ "${{COMP_WORDS[1]}}" = "rm" ]; then
                     COMPREPLY=($(compgen -W "$(ls {0})" -- "${{COMP_WORDS[${{COMP_CWORD}}]}}"))
+                elif [ "${{COMP_WORDS[1]}}" = "unlink" ]; then
+                    COMPREPLY=($(compgen -W "$(ls {0})" -- "${{COMP_WORDS[${{COMP_CWORD}}]}}"))
+                elif [ "${{COMP_WORDS[1]}}" = "use" ]; then
+                    COMPREPLY=($(compgen -W "$(ls {0})" -- "${{COMP_WORDS[${{COMP_CWORD}}]}}"))
                 fi
             }}
 
@@ -53,19 +59,26 @@ impl ShellOperation for CompletionOperation {
         format!(
             r#"
             complete -ec venv
-            set -l venv_wrapper_commands activate help init ls new rm
+            set -l venv_wrapper_commands activate help init link ls new rm unlink use
             set venv_wrapper_split_path (string split : $PATH)
 
             complete -f -c venv
             complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a activate -d "activate a virtualenv"
             complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a help -d "show the help text"
+            complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a link -d "link a project directory with a virtualenv"
             complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a ls -d "list available virtualenvs"
             complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a new -d "create a new virtualenv"
             complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a rm -d "remove an existing virtualenv"
+            complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a unlink -d "unlink a project directory from a virtualenv"
+            complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -a use -d "use a virtualenv (cd to the project and activate)"
             complete -f -c venv -n "not __fish_seen_subcommand_from $venv_wrapper_commands" -s r -l venv-root -a "(ls -ad */ .*/)" -d "set the virtualenv root"
 
             complete -f -c venv -n "__fish_seen_subcommand_from activate" -a "(ls {0})"
             complete -f -c venv -n "__fish_seen_subcommand_from rm" -a "(ls {0})"
+            complete -f -c venv -n "__fish_seen_subcommand_from link" -a "(ls {0})"
+            complete -F -c venv -n "__fish_seen_subcommand_from link" -s p -l project -a "(__fish_complete_directories)"
+            complete -f -c venv -n "__fish_seen_subcommand_from unlink" -a "(ls {0})"
+            complete -f -c venv -n "__fish_seen_subcommand_from use" -a "(ls {0})"
             complete -f -c venv -n "__fish_seen_subcommand_from new" -s p -l python -a "(command find $venv_wrapper_split_path -executable -name 'python*' 2>/dev/null | command grep -o 'python\([0-9]\|[0-9]\.[0-9]\|[0-9]\.[0-9]\.[0-9]\)\?\$' | sort -u)"
 
             set -e venv_wrapper_commands
@@ -95,8 +108,12 @@ impl ShellOperation for CompletionOperation {
                     '-r[venv root]' '--venv-root[venv root]' \
                     '1: :((activate\:"activate a virtualenv"
                            help\:"show the help text"
+                           link\:"link a project directory with a virtualenv"
                            ls\:"list available virtualenvs"
-                           new\:"create a new virtualenv"))' \
+                           new\:"create a new virtualenv"
+                           rm\:"remove an existing virtualenv"
+                           unlink\:"unlink a project directory from a virtualenv"
+                           use\:"use a virtualenv (cd to the project and activate)"))' \
                     '*::arg:->args'
 
                 case "$line[1]" in
@@ -105,6 +122,12 @@ impl ShellOperation for CompletionOperation {
                     ;;
                     help)
                         return 0
+                    ;;
+                    link)
+                        _arguments \
+                            '-p[project directory]:project directory:_path_files -/' \
+                            '--python[project cirectory]:project directory:_path_files -/' \
+                            ':virtualenv name:($(_venv_wrapper_available_virtualenvs))'
                     ;;
                     ls)
                         return 0
@@ -116,6 +139,12 @@ impl ShellOperation for CompletionOperation {
                             ':virtualenv name:()'
                     ;;
                     rm)
+                        _describe 'available virtualenvs' "($(_venv_wrapper_available_virtualenvs))"
+                    ;;
+                    unlink)
+                        _describe 'available virtualenvs' "($(_venv_wrapper_available_virtualenvs))"
+                    ;;
+                    use)
                         _describe 'available virtualenvs' "($(_venv_wrapper_available_virtualenvs))"
                     ;;
                 esac
