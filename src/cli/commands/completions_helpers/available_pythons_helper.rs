a914425ddfd779deb17eec2d271a98e61fc0c26e
Nicolas Marier <software@nmarier.com>
2021-08-01 10:52:13 -0400

    refactor(available-pythons-helper): use which-rs to find available pythons

--- src/cli/commands/completions_helpers/available_pythons_helper.rs
+++ src/cli/commands/completions_helpers/available_pythons_helper.rs
@@ -1,8 +1,7 @@
-use std::{env, fs, path::PathBuf};
-
 use clap::ArgMatches;
-use eyre::{Context, Result};
+use eyre::Result;
 use regex::Regex;
+use which::which_re;
 
 use crate::{
     cli::command::{Command, CommandResult},
@@ -13,7 +12,7 @@ use crate::{
 pub struct AvailablePythonsHelper;
 impl Command for AvailablePythonsHelper {
     fn run(_config: &Config, _matches: &ArgMatches) -> Result<CommandResult> {
-        let re = r"^python\d?(?:\.\d+(?:\.\d+)?)?$";
+        let re = Regex::new(r"^python\d?(?:\.\d+(?:\.\d+)?)?$")?;
 
         let mut python_interpreters: Vec<String> =
             which_re(re)?.map(|p| p.file_name().unwrap().to_string_lossy().to_string()).collect();
@@ -22,32 +21,4 @@ impl Command for AvailablePythonsHelper {
 
         Ok(CommandResult::new().output(Box::new(intersperse(python_interpreters.iter(), ' '))))
     }
-}
-
-fn which_re(re: &str) -> Result<impl Iterator<Item = PathBuf>> {
-    let path = env::var("PATH").wrap_err("No PATH environment variable defined")?;
-    let re = Regex::new(re).wrap_err("Invalid regex supplied")?;
-
-    let valid_paths = path.split(':').map(PathBuf::from).filter(|p| p.exists() && p.is_dir());
-
-    let mut results = Vec::new();
-    for bin_dir in valid_paths {
-        let entries = match fs::read_dir(bin_dir) {
-            Ok(e) => e,
-            Err(_) => continue,
-        };
-
-        for entry in entries {
-            let entry = match entry {
-                Ok(e) => e,
-                Err(_) => continue,
-            };
-
-            if re.is_match(&entry.file_name().to_string_lossy()) {
-                results.push(entry.path());
-            }
-        }
-    }
-
-    Ok(results.into_iter())
 }
